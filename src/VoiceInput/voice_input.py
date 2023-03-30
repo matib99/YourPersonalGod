66a6a49c8fcdf2dc859e5d524eeb328fcfddcbfb
Mateusz Błajda <mateuszblajda99@gmail.com>
2023-03-30 18:25:01 +0200

    import fixes

--- src/VoiceInput/voice_input.py
+++ src/VoiceInput/voice_input.py
@@ -1,14 +1,5 @@
-import argparse
-import io
-import os
 import speech_recognition as sr
-import whisper
-import torch
 
-from datetime import datetime, timedelta
-from queue import Queue
-from tempfile import NamedTemporaryFile
-from time import sleep
 from sys import platform
 
 
